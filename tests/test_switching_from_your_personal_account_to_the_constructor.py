ea90678ce85c0d91a4740088752c54d5fc6da5e4
Александр Абрамов <aabramov06@yandex.ru>
2026-01-10 14:42:43 +0500

    2026.01.10-13-40!!

--- tests/test_switching_from_your_personal_account_to_the_constructor.py
+++ tests/test_switching_from_your_personal_account_to_the_constructor.py
@@ -26,7 +26,7 @@ class Test_switching_from_our_personal_account_to_the_constructor():
         # Нажимаем кнопку "Зарегистрироваться."
         driver.find_element(*Locators.BUTTON_REGISTER).click()
         # Ожидаем, что перешли на форму входа.
-        time.sleep(1)
+       
         # Заполняем форму входа в аккаунт.
         driver.find_element(*Locators.EMAIL_EDIT_LOGIN).clear()
         driver.find_element(*Locators.EMAIL_EDIT_LOGIN).send_keys(login)
